53bbc1e3f7cd3343304e3c3ea6fbf2c850dc1b79
John Paolo Flores <john.paolo.flores@oocl.com>
2019-01-17 11:48:15 +0800

    added redux form + binded signin method

--- src/components/login/index.js
+++ src/components/login/index.js
@@ -5,9 +5,22 @@ import { Col, Row, Grid } from 'react-native-easy-grid';
 import { createStackNavigator, createAppContainer } from 'react-navigation';
 import Home from 'components/home';
 import Style from './styles';
-import styles from './styles';
+
 
 class Login extends Component {
+state = { username: 'owpao', password: 'owpao', loading: false };
+
+  onSignin = () => {
+    if (this.state.username == 'owpao' && this.state.password == 'owpao') {
+      this.props.navigation.navigate('Home');
+    } else {
+      console.log("error");
+    }
+
+  };
+
+  
+
   render() {
     return (
       <Container style={Style.loginContainer}>
@@ -18,18 +31,29 @@ class Login extends Component {
             <Form style={Style.loginForm}>
               <Item rounded >
                 <Icon name='user' type='AntDesign' />
-                <Input placeholder='Username' />
+                <Input
+                  placeholder='Username'
+                  label='username'
+                  value={this.state.username}
+                  onChangeText={username => this.setState({ username })}
+                />
               </Item>
 
               <Item rounded>
                 <Icon name='lock' type='Feather' />
-                <Input placeholder='Password' secureTextEntry />
+                <Input
+                  placeholder='Password'
+                  secureTextEntry
+                  label='password'
+                  value={this.state.password}
+                  onChangeText={password => this.setState({ password })}
+                />
               </Item>
             </Form>
             <Grid style={Style.loginGrid}>
               <Row>
                 <Col>
-                  <Button block rounded style={Style.loginSignin} onPress={() => this.props.navigation.navigate('Home')}>
+                  <Button block rounded style={Style.loginSignin} onPress = {this.onSignin.bind(this)}>
                     <Text> Login </Text>
                   </Button>
                 </Col>
