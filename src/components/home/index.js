72428a7ea0ca7bc911827c865d5e36fbdf9d1f50
John Paolo Flores <john.paolo.flores@oocl.com>
2019-01-18 11:35:35 +0800

    Added pick up point / Destination text box

--- src/components/home/index.js
+++ src/components/home/index.js
@@ -1,12 +1,13 @@
-import React, {Component} from 'react';
+import React, { Component } from 'react';
 
 import {
-    Container, Header, Left, Button, Icon, Right, Body, Title, Drawer
+    Container, Header, Left, Button, Icon, Right, Body, Title, Drawer, Input, Item, InputGroup, Grid, Row, Col, Card, CardItem, Text, View
 } from 'native-base';
 
 import Sidebar from 'components/sidebar';
-import {Styles} from 'constants/styles';
+import { Styles } from 'constants/styles';
 import MapView from 'components/map';
+import { Modal } from 'react-native';
 
 export default class Home extends Component {
     closeDrawer = () => {
@@ -22,23 +23,42 @@ export default class Home extends Component {
                 ref={(ref) => {
                     this.drawer = ref;
                 }}
-                content={<Sidebar/>}
+                content={<Sidebar />}
                 onClose={() => this.closeDrawer()}>
 
                 <Container>
-                    <Header style={Styles.statusBar}/>
-                    <Header style={{backgroundColor: "#212121"}}>
+                    <Header style={Styles.statusBar} />
+                    <Header style={{ backgroundColor: "#212121" }}>
                         <Left>
                             <Button transparent onPress={() => this.openDrawer()}>
-                                <Icon name='menu'/>
+                                <Icon name='menu' />
                             </Button>
                         </Left>
                         <Body>
-                        <Title>ATRS</Title>
+                            <Title>ATRS</Title>
                         </Body>
-                        <Right/>
+                        <Right />
                     </Header>
-                    <MapView/>
+
+                    <View style={{ alignContent: "center", justifyContent: "space-evenly", alignItems: "center", height: 100 }}>
+                        <Item style={{ height: '50%', paddingHorizontal:10, backgroundColor:'#f5f6fa' }}>
+                            <Icon style={{ color: '#e84118' }} name='place' type='MaterialIcons' />
+                            <Input
+                                placeholder='Pick-up point'
+                                label='pickup'
+                            />
+                        </Item>
+
+                        <Item style={{ height: '50%', paddingHorizontal:10, backgroundColor:'#f5f6fa' }}>
+                            <Icon style={{ color: '#4cd137' }} name='place' type='MaterialIcons' />
+                            <Input
+                                placeholder='Destination'
+                                label='destination'
+                            />
+                        </Item>
+                    </View>
+
+                    <MapView />
                 </Container>
             </Drawer>
         );
