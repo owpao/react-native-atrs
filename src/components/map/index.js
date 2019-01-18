1e70c648288a321de81cef89e28354208f070181
John Paolo Flores <john.paolo.flores@oocl.com>
2019-01-18 11:35:57 +0800

    Marker implementation + onRegionChange event on Mapview

--- src/components/map/index.js
+++ src/components/map/index.js
@@ -1,19 +1,57 @@
 import React from 'react';
 import { MapView } from 'expo';
+import { Container } from 'native-base';
+import { Marker } from 'react-native-maps';
 
 export default class App extends React.Component {
+    state = {
+        region: {
+            latitude: 14.5248595,
+            longitude: 121.0001437,
+            latitudeDelta: 0.0922,
+            longitudeDelta: 0.0421,
+        }
+    };
+    getInitialState() {
+        return {
+            region: {
+                latitude: 14.5248595,
+                longitude: 121.0001437,
+                latitudeDelta: 0.0922,
+                longitudeDelta: 0.0421,
+            },
+        };
+    }
+
+    onRegionChange(region) {
+        this.setState({ region });
+    }
 
     render() {
         return (
-            <MapView
-                style={{ flex: 1 }}
-                initialRegion={{
-                    latitude: 14.5248595,
-                    longitude: 121.0001437,
-                    latitudeDelta: 0.0922,
-                    longitudeDelta: 0.0421,
-                }}
-            />
+            <Container>
+                <MapView
+                    style={{ flex: 1 }}
+                    initialRegion={{
+                        latitude: 14.5248595,
+                        longitude: 121.0001437,
+                        latitudeDelta: 0.0922,
+                        longitudeDelta: 0.0421,
+                    }}
+                    showsUserLocation={true}
+                    followUserLocation={true}
+                    zoomEnabled={true}
+                    onRegionChange={this.onRegionChange.bind(this)}
+                >
+                    <Marker
+                        coordinate={{ latitude: this.state.region.latitude, longitude: this.state.region.longitude }}
+                        title="Sample"
+                        description="Sample Description"
+                    />
+                </MapView>
+            </Container>
+
+
         );
     }
 }
